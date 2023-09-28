b4de86bd8b5911c14c6148e3a7e8db723e9ba39d
Ankit Kumar <ankitgeotek@gmail.com>
2023-09-28 17:22:27 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.title("CampusX")
+
+col1, col2 =st.columns(2)
+
+with col1:
+    st.image("sample1.jpeg")
+
+with col2:
+    st.write(
+        """"
+        India’s digital public infrastructure (DPI), loosely the India Stack and more, is shaped in a unique partnership between governments (Union and States), regulators, the private sector, selfless volunteers, startups, and academia/think tanks.
+India, through India Stack, became the first country to develop all three foundational DPIs: digital identity (Aadhar), real-time fast payment (UPI) and a platform to safely share personal data without compromising privacy. 
+        """
+    )
